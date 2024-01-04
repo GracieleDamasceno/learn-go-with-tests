cb364db0321ab08a98a08a1943946d40a92ea475
GracieleDamasceno <damasceno.graciele@gmail.com>
2024-01-03 22:27:22 -0300

    Hello, World - 2

--- hello.go
+++ hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello world in go!"
+const (
+	englishHelloPrefix = "Hello "
+	spanishHelloPrefix = "Hola "
+	koreanHelloPrefix  = "안녕하세요 "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	return getGreetings(language) + name
+}
+
+func getGreetings(language string) (prefix string) {
+	switch language {
+	case "korean":
+		prefix = koreanHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Joe", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,34 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world in go!"
+	t.Run("say hello to someone", func(t *testing.T) {
+		got := Hello("Joe", "")
+		want := "Hello Joe"
 
+		assertStrings(got, want, t)
+	})
+	t.Run("say hello world when name is empty", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello world"
+
+		assertStrings(got, want, t)
+	})
+	t.Run("say hello in spanish", func(t *testing.T) {
+		got := Hello("Bobby", "spanish")
+		want := "Hola Bobby"
+
+		assertStrings(got, want, t)
+	})
+	t.Run("say hello in korean", func(t *testing.T) {
+		got := Hello("Jennie", "korean")
+		want := "안녕하세요 Jennie"
+
+		assertStrings(got, want, t)
+	})
+}
+
+func assertStrings(got string, want string, t testing.TB) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
