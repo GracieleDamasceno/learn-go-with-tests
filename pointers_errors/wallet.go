87a6a77f2e1a07ddb72f7bee38c94b91f2296d39
GracieleDamasceno <damasceno.graciele@gmail.com>
2024-01-09 21:53:39 -0300

    Point & Errors 2

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,13 +1,23 @@
 package pointerserrors
 
+import "errors"
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("insuficient funds")
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -3,18 +3,48 @@ package pointerserrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
+	t.Run("depositing bitcoin", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := 10
+		got := wallet.Balance().String()
+		want := Bitcoin(10).String()
 
-	assertIntegers(got, want, t)
+		assertStrings(got, want, t)
+	})
+
+	t.Run("withdraw bitcoin", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance().String()
+		want := Bitcoin(10).String()
+
+		assertStrings(got, want, t)
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		error := wallet.Withdraw(Bitcoin(100))
+
+		assertError(error, "insuficient funds", t)
+	})
+
 }
 
-func assertIntegers(got int, want int, t testing.TB) {
+func assertStrings(got string, want string, t testing.TB) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(got error, want string, t testing.TB) {
+	if got == nil {
+		t.Fatal("Expected an error, got none")
+	}
+
+	if got.Error() != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
